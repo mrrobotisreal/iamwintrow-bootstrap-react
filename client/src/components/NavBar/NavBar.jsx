810ca4e890162d10f2bf16ddc911133d25708a58
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-11 13:25:53 -0600

    Changed App.css to NavBar.css and moved it inside the NavBar directory, imported it into NavBar component.

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import './NavBar.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Navbar } from 'react-bootstrap';
 
 export default function NavBar() {
   return (
     <div>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="myRed" variant="dark">
         <Navbar.Brand>
           Logo
         </Navbar.Brand>
